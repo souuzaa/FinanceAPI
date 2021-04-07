2b69af9582b96123f80943f22b8d693cb7c406cc
Rafael Pinto <rafaelpinto@Rafaels-MacBook-Pro.local>
2021-04-07 13:44:28 -0300

    Initial project

---
+++ FinanceAPI/Controllers/FinanceController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinanceAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FinanceAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FinanceController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "APPL", "MSFT", "AMZN", "FDX"
+        };
+
+        private readonly ILogger<FinanceController> _logger;
+
+        public FinanceController(ILogger<FinanceController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Stock> GetList()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 4).Select(index => new Stock
+            {
+                Date = DateTime.Now.AddDays(index),
+                LastPrice = (double)rng.Next(20, 55),
+                Ticker = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ FinanceAPI/Model/Stock.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace FinanceAPI.Model
+{
+    public class Stock
+    {
+        public DateTime Date { get; set; }
+
+        public string Ticker { get; set; }
+
+        public double LastPrice { get; set; }
+    }
+}
